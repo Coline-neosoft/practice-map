f1c12b4e7eb29b2a67582af9df83984ac732b857
Lucie MENAGE <l.menage@NeoSoft.local>
2024-03-06 11:19:33 +0100

    started adding rooms

--- tilesets/spritesheet.tsx
+++ tilesets/spritesheet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="spritesheet" tilewidth="32" tileheight="32" tilecount="49" columns="7">
+<tileset version="1.10" tiledversion="1.10.2" name="spritesheet" tilewidth="23" tileheight="23" tilecount="81" columns="9">
  <image source="spritesheet.png" width="224" height="224"/>
 </tileset>
